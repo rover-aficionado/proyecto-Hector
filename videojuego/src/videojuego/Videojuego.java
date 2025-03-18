be64a8d2e285a2b786514bca2d604096a43b1412
roberto <robertogarciahuidobro@gmail.com>
2025-03-18 11:05:56 +0100

    inicio del proyecto

---
+++ videojuego/src/videojuego/Videojuego.java
@@ -0,0 +1,13 @@
+package videojuego;
+
+/**
+ *
+ * @author rober
+ */
+public class Videojuego {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
