91c0f3353f9f5d010e1d57ac81e053742a2ce8bf
roberto <robertogarciahuidobro@gmail.com>
2025-03-27 11:49:03 +0100

    inicio de sistema de guardado en una base de datos

---
+++ videojuego/src/utils/GuardarDB.java
@@ -0,0 +1,15 @@
+/*
+* @author Roberto García
+* @date 27 mar 2025 at 11:42:50
+* Guarda las partidas en una base de datos adjunta (bbdd.txt)
+*/
+
+package utils;
+
+import java.util.Scanner;
+
+
+public class GuardarDB {
+    String url = "\"jdbc:mysql://127.0.0.1:3306/prueba\"";
+    Scanner scn = new Scanner(System.in);
+}
