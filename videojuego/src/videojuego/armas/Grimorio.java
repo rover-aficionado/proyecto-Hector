7735d4d1f795bf2b250bc669b73a5a4c636e4197
roberto <roberto@gmail.com>
2025-04-08 13:55:57 +0200

    nuevo aviso en la base de datos. la contraseña tiene que ser cambiada a la contraseña del servidor local

--- videojuego/src/videojuego/armas/Grimorio.java
+++ videojuego/src/videojuego/armas/Grimorio.java
@@ -1,7 +1,7 @@
 package videojuego.armas;
 
 
-public class Grimorio extends Armas{
+public class Grimorio extends Armas{ //esto es un libro d ehechizos. Es un nombre de esos que uno no sabe que existe pero que resulta que exisye
     
     public Grimorio(String nombre, double daño, int precio) {
         super(nombre, daño, precio);
