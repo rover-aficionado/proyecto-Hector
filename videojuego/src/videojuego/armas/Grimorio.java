86034774d894e6103792e428092d1c72a7463644
roberto <roberto@gmail.com>
2025-04-09 09:43:40 +0200

    nuevo constructor para el grimorio

--- videojuego/src/videojuego/armas/Grimorio.java
+++ videojuego/src/videojuego/armas/Grimorio.java
@@ -1,15 +1,38 @@
 package videojuego.armas;
 
 
-public class Grimorio extends Armas{ //esto es un libro d ehechizos. Es un nombre de esos que uno no sabe que existe pero que resulta que exisye
+public class Grimorio extends Armas{ //esto es un libro de hechizos. Es un nombre de esos que uno no sabe que existe pero que resulta que exisye
     
-    public Grimorio(String nombre, double daño, int precio) {
+    double potenciador; // el potenciador es la habilidad temporal que va a aprender el jugador. como la fuerza extra
+    int tiempo; 
+
+    public Grimorio(double potenciador, String nombre, double daño, int precio, int tiempo) {
         super(nombre, daño, precio);
+        this.potenciador = potenciador;
+        this.tiempo=tiempo;
+    }
+
+    public double getPotenciador() {
+        return potenciador;
+    }
+
+    public void setPotenciador(double potenciador) {
+        this.potenciador = potenciador;
     }
 
     @Override
     public String toString() {
-        return super.toString() + " Grimorio{" + '}';
+        return "Grimorio{" + "potenciador=" + potenciador + '}';
     }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+    
+    
     
 }
